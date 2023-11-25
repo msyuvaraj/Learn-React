19d4d4de02569438b1a1d13856d53780f097f1d9
msyuvaraj <msyuvaraj1@gmail.com>
2023-11-25 20:09:30 +0530

    added notes

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello from React"
-);
+const jsxHeading = <h1 className="heading">Namste React using jsx</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
